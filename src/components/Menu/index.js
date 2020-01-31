c8ccddcf2092ea62736300400a9e3c4503ff515b
Ryann Ferreira <ryannnkl@gmail.com>
2020-01-31 22:42:23 +0000

    push via gitpod

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert } from 'react-native';
+import { Alert, View } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 
 import { Container, Code, Nav, NavItem, NavText, SingOut, SingOutText, QRCode } from './styles';
@@ -12,26 +12,40 @@ export default function Menu({ translateY }){
         outputRange: [0,1],
         extrapolate: 'clamp'
       })
-    }} >
+    }} 
+			showsVerticalScrollIndicator={false}
+		>
       <Code>
          <QRCode /> 
       </Code>
       <Nav>
         <NavItem>
-          <FontAwesome5 name="question-circle" size={20} color="#fff" />
-          <NavText>Me ajuda</NavText>
+          <View style={{flexDirection: 'row'}}>
+						<FontAwesome5 name="question-circle" size={20} color="#fff" style={{width: 24}}/>
+						<NavText>Me ajuda</NavText>
+					</View>
+          <FontAwesome5 name="angle-right" size={24} color="#f6f6f6"/>
         </NavItem>
         <NavItem>
-          <FontAwesome5 name="user" size={20} color="#fff" />
-          <NavText>Perfil</NavText>
+					 <View style={{flexDirection: 'row'}}>
+						<FontAwesome5 name="user" size={20} color="#fff" style={{width: 24}}/>
+						<NavText>Perfil</NavText>
+					</View>
+					 <FontAwesome5 name="angle-right" size={24} color="#f6f6f6"/>
         </NavItem>
         <NavItem>
-          <FontAwesome5 name="credit-card" size={20} color="#fff" />
-          <NavText>Configurar cartão</NavText>
+					<View style={{flexDirection: 'row', }} >
+						<FontAwesome5 name="credit-card" size={20} color="#fff" style={{width: 24}} />
+						<NavText>Configurar cartão</NavText>
+					</View>
+					 <FontAwesome5 name="angle-right" size={24} color="#f6f6f6"/>
         </NavItem>
         <NavItem>
-          <FontAwesome5 name="mobile-alt" size={20} color="#fff" />
-          <NavText>Configuração do app</NavText>
+					<View style={{flexDirection: 'row'}}>
+						<FontAwesome5 name="mobile-alt" size={20} color="#fff" style={{width: 24}}/>
+						<NavText>Configuração do app</NavText>
+					</View>
+					 <FontAwesome5 name="angle-right" size={24} color="#f6f6f6"/>
         </NavItem>
       </Nav>
       <SingOut onPress={()=>{ Alert.alert('saiu','apenas um teste') }}>
